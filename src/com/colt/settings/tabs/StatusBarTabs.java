b28972190a73ee733bf8fffd37123441dcfe7129
Lars Greiss <kufikugel@googlemail.com>
2013-11-06 00:18:39 +0100

    Statusbar clock customization (2/2)
    - added slims clock customizations for status bar
    - added clock seconds from aosp
    
    Change-Id: I461125b7e2e05b2d1e274745ea8fbca4d366182d
    Signed-off-by: Dragon-oniv <nvrevrgivup@gmail.com>

--- src/com/colt/settings/tabs/StatusBarTabs.java
+++ src/com/colt/settings/tabs/StatusBarTabs.java
@@ -34,6 +34,7 @@ import com.colt.settings.PagerSlidingTabStrip;
 import com.colt.settings.fragments.statusbartabs.StatusBarMisc;
 import com.colt.settings.fragments.statusbartabs.NotificationTicker;
 import com.colt.settings.fragments.statusbartabs.NetworkTraffic;
+import com.colt.settings.fragments.statusbartabs.StatusBarClockSettings;
 
 public class StatusBarTabs extends SettingsPreferenceFragment {
 
@@ -86,7 +87,8 @@ public class StatusBarTabs extends SettingsPreferenceFragment {
             super(fm);
             frags[0] = new StatusBarMisc();
             frags[1] = new NotificationTicker();
-            frags[2] = new NetworkTraffic();
+            frags[2] = new StatusBarClockSettings();
+            frags[3] = new NetworkTraffic();
         }
 
         @Override
@@ -110,6 +112,7 @@ public class StatusBarTabs extends SettingsPreferenceFragment {
         titleString = new String[]{
                 getString(R.string.statusbar_misc_title),
                 getString(R.string.notification_ticker_title),
+                getString(R.string.statusbar_clock_title),
                 getString(R.string.network_traffic_title)
 	};
         return titleString;
